25fc9f9d19b39e7a768f9c255d5c4b44be2b8616
Abiodun Bolujo <l00186151@atu.ie>
2024-01-28 20:40:04 +0000

    adding homepage object

---
+++ GlobalGreenInitiative-UIAuto/src/main/java/com/group2/pages/homePage.java
@@ -0,0 +1,31 @@
+package com.group2.pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class homePage {
+
+    public homePage open() {
+        Selenide.open("http://localhost:8080/homepage/new");
+        return this;
+    }
+
+    public homePage searchFor(String text) {
+        $(By.xpath("//html/body/div[1]/form/div/p[1]")).val(text).pressEnter();
+        return this;
+    }
+
+    public void clickText(String text){
+        $(By.linkText(text)).pressEnter();
+
+    }
+    public ElementsCollection results() {
+        return $$(".srg .g");
+    }
+
+
+}
