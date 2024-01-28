3d877dd38614bf5d31bb4a39e7549dd705d0f643
Abiodun Bolujo <l00186151@atu.ie>
2024-01-28 21:54:02 +0000

    adding customerFormpage object

---
+++ GlobalGreenInitiative-UIAuto/src/main/java/com/group2/pages/customerForm.java
@@ -0,0 +1,21 @@
+package com.group2.pages;
+
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class customerForm {
+
+    public void enterDetails(String fname, String lname, String address, String dob){
+        $(By.id("firstName")).setValue(fname);
+        $(By.id("lastName")).setValue(lname);
+        $(By.id("address")).setValue(address);
+        $(By.id("DOB")).setValue(dob);
+        sleep(2000);
+        $(By.xpath("//button[text()='Submit']")).click();
+    }
+
+
+}
